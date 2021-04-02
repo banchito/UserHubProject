2ee64533bc3af079c6cea412f4446c58b0a4662c
Esteban Ordonez <estebanusco@gmail.com>
2021-04-01 21:12:08 -0700

    Module 3 uo to SetCommentsOnPost

--- app.js
+++ app.js
@@ -4,43 +4,57 @@ const users_URL = `${BASE_URL}/users`;
 
 const queryObj = {
   _expand: "user",
-  _embed: "photos"
+  _embed: "photos",
 };
 
-const QUERY = Object.entries(queryObj)
+// Build the query for user posts
+const query_user_posts = Object.entries(queryObj).map(function (line) {
+  return line.join("=");
+});
+
+// Build the query for album List
+const query_album_list = Object.entries(queryObj)
   .map(function (line) {
     return line.join("=");
   })
   .join("&");
 
-
 // Fetching data with url provided by helper func's
 function fetchData(url) {
-    return fetch(url)
-    .then(function(response){
-        return response.json()
+  return fetch(url)
+    .then(function (response) {
+      return response.json();
     })
-    .catch(function(error){
-        console.error(error)
-    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
 
-// Helper Function fetching users 
+// Helper Function fetching users
 function fetchUsers() {
-  return fetchData(users_URL)
+  return fetchData(users_URL);
 }
 
 // Helper Function fetching users' albums
 function fetchUserAlbumList(userId) {
-  return fetchData(`${users_URL}/${userId}/albums?${QUERY}`)  
+  return fetchData(`${users_URL}/${userId}/albums?${query_album_list}`);
 }
 
-//fetchUserAlbumList(1).then(renderAlbumList) 
+// Helper Function fetching users' posts
+function fetchUserPosts(userId) {
+  return fetchData(`${users_URL}/${userId}/posts?${query_user_posts[0]}`);
+}
 
+// Helper Function fetching posts' comments
+function fetchPostComments(postId) {
+  return fetchData(`${BASE_URL}/posts/${postId}/comments`);
+}
 
+fetchUserPosts(1).then(console.log); 
 
+fetchPostComments(1).then(console.log)
 
-// Templating
+// Templating users
 function renderUser(user) {
   //header
   let name = user.name;
@@ -78,77 +92,74 @@ function renderUser(user) {
 // render an array of users
 function renderUserList(userList) {
   $("#user-list").empty();
+
   userList.forEach(function (user) {
     const userElem = renderUser(user);
     $("#user-list").append(userElem);
   });
 }
 
-/* render a single album */
+// Templating a single album
 function renderAlbum(album) {
-    let albumElem = 
-    `<div class="album-card">
+  const albumElem = $(`<div class="album-card">
      <header>
-       <h3>${album.title} </h3>
+       <h3>${album.title}, by ${album.user.username} </h3>
      </header>
-     <section class="photo-list">
-     </section>
-   </div>`
+     <section class="photo-list"></section>
+   </div>`);
 
-    album.photos.forEach(function(photo){
-        const photoElem = renderPhoto(photo)
-        $('.photo-list').append(photoElem)
-    })
+  const photoListClass = albumElem.find(".photo-list");
 
-   return albumElem
-}
-// render an array of albums 
-function renderAlbumList(albumList) {
-    
-    $('#app section.active').removeClass('active')
-    $('#album-list').addClass('active')
-    $('#album-list').empty()
-
-    albumList.forEach(function(album){
-        const albumElem = renderAlbum(album)
-        $('#album-list').append(albumElem)
-    })
+  album.photos.forEach(function (photo) {
+    const photoElem = renderPhoto(photo);
+    photoListClass.append(photoElem);
+  });
+  return albumElem;
 }
 
-// render a single photo 
+// render a single photo
 function renderPhoto(photo) {
-    let photoElem = 
-    `<div class="photo-card">
-        <a href=${photo.url} target="_blank">
-          <img src=${photo.url}>
+  return `<div class="photo-card">
+        <a href="${photo.Url}" target="_blank">
+          <img src="${photo.thumbnailUrl}">
           <figure>${photo.title}</figure>
         </a>
-    </div>`
-    return photoElem
+    </div>`;
 }
 
+// render an array of albums
+function renderAlbumList(albumList) {
+//   console.log(albumList);
+  $("#app section.active").removeClass("active");
+  $("#album-list").empty().addClass("active");
 
-//simplified way of chainnig the promise from fecth user:
-//fetchUsers().then(renderUserList);
+  albumList.forEach(function (album) {
+    const albumElem = renderAlbum(album);
+    $("#album-list").append(albumElem);
+  });
+}
+
 function bootstrap() {
   fetchUsers().then(function (data) {
-    console.log(data);
+    //console.log(data);
     renderUserList(data);
   });
-  
 }
 
+//Event Listeners
+
+//Loading Posts
 $("#user-list").on("click", ".user-card .load-posts", function () {
   let parent = $(this).closest(".user-card");
-  console.log(parent.data("user"));
+//   console.log(parent.data("user"));
 });
 
+//Loading Albums
 $("#user-list").on("click", ".user-card .load-albums", function () {
-  let parent = $(this).closest(".user-card");
-  let userID = parent.data("user").id
-  
-  fetchUserAlbumList(userID).then(renderAlbumList)
-  
+  let parent = $(this).closest(".user-card").data("user");
+  fetchUserAlbumList(parent.id).then(function (albumList) {
+    renderAlbumList(albumList);
+  });
 });
 
 bootstrap();
